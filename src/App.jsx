39704c7c34549416e6d357404684e69cfdff5364
Soa <hei.soa.3@gmail.com>
2025-07-06 00:55:35 +1300

    Background upload

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
       <div>
         <nav
           className="bg-[#001948] text-white 
-           top-0 absolute w-full flex items-center p-[10px] h-3xs"
+           top-0 absolute w-full flex items-center p-[1px] h-3xs z-30"
         >
           <img src={vraiLogoHEI} alt="" className="w-[125px] p-0 h-auto" />
           <ul className="flex gap-6 items-center text-white ml-[37%]  ">
@@ -55,12 +55,47 @@ function App() {
           </ul>
         </nav>
 
-        <img
-          src={bg3}
-          alt="background image "
-          className="w-screen h-screen object-cover max-h-[600px] mx-auto z-0"
-        />
-        
+        <div>
+          <img
+            src={bg3}
+            alt="background image "
+            className="w-screen h-screen object-cover max-h-[600px] mx-auto z-0"
+          />
+          <div className="absolute inset-0 bg-black/50 z-10"></div>
+          <div className="absolute inset-0 z-20 flex items-center justify-center">
+            <h1
+              className="text-white mb-[100px] text-5xl font-serif
+             font-bold flex items-center justify-center"
+            >
+              Haute école d'Informatique
+            </h1>
+            <div className="absolute inset-0 z-20 flex items-center justify-center">
+              <h2 className="text-white font-serial   mt-[25ch] p-[70px] m-[45PX]  ">
+                "L'éducation est l'arme la plus puissante pour changer le
+                monde." selon Nelson Mandela "L'éducation dans l'informatique
+                est une arme puissante pour lutter contre la pauvreté à
+                Madagascar"selon HEI. C'est notre mission.
+              </h2>
+            </div>
+            <div className="absolute inset-0 z-20 flex items-center justify-center">
+              <button className=" absolute mt-[450px] mr-[720px] font-bold text-xl  ">
+                Inscrivez-vous ici
+              </button>
+              <button
+                className="absolute mt-[450px] mr-[0px] 
+              font-bold text-xl bg-[#DFA408] text-white "
+              >
+                Emploie du temps
+              </button>
+              <button
+                className="absolute mt-[450px] ml-[650px] 
+              font-bold text-xl bg-[#001948] text-white "
+              >
+                Programme pédagogique
+              </button>
+            </div>
+          </div>
+        </div>
 
         <button className="bg-secondary text-primary px-4 py-2 rounded">
           Bouton stylé
